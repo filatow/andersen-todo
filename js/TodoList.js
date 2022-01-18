2e82c1c413a573c0bb48f2406fcbe977f73f6cd8
evgeniy <not-found@inbox.ru>
2022-01-18 13:43:21 +0300

    US002UI: Added possibility to add new items

--- js/TodoList.js
+++ js/TodoList.js
@@ -1,17 +1,13 @@
 import { TodoItem } from './TodoItem.js';
+import { POSITION_TO_INSERT as POSITION, KEY_CODE } from './consts';
 
 export class TodoList {
   #container;
-  #todoItems;
-  #todoItemComponents;
+  #todoItems = [];
 
   constructor(container, items = []) {
     this.#container = container;
-    this.#todoItems = items;
-    this.#todoItemComponents = this.#todoItems.map(
-      (item) => new TodoItem(item)
-    );
-
+    this.add(...items);
     this.render();
   }
 
@@ -21,34 +17,78 @@ export class TodoList {
     return todoListMarkup;
   }
 
+  #getNewItemInputGroupMarkup() {
+    const newItemInputGroupMarkup = `
+    <div class="input-group mb-3">
+      <input
+        type="text" class="form-control"
+        id="new-item-input" placeholder="New task"
+      >
+      <button class="btn btn-warning fs-5" type="button">+</button>
+    </div>`;
+    
+    return newItemInputGroupMarkup;
+  }
+
   #listItemClickHandler = (evt) => {
-    const currentLabel = evt.target.closest('label');
-    if (!currentLabel) return;
+    const clickedLabel = evt.target.closest('label');
+    if (!clickedLabel) return;
 
-    const currentLi = evt.target.closest('li');
-    if (!currentLi) return;
+    const clickedItemElement = evt.target.closest('li');
+    if (!clickedItemElement) return;
 
-    const componentToUpdate = this.#todoItemComponents.find(
-      (component) => component.getId() === currentLi.dataset.id
+    const itemToUpdate = this.#todoItems.find(
+      (item) => item.getId() === clickedItemElement.dataset.id
     );
-    componentToUpdate.isDone = !componentToUpdate.isDone;
-    currentLi.insertAdjacentHTML('afterend', componentToUpdate.getMarkup());
-    currentLi.remove();
+
+    itemToUpdate.isDone = !itemToUpdate.isDone;
+    clickedItemElement.insertAdjacentHTML(
+      POSITION.AFTER_END,
+      itemToUpdate.getMarkup()
+    );
+    clickedItemElement.remove();
+  };
+
+  #enterKeydownHandler = (evt) => {
+    const newItemInput = this.#container.querySelector('#new-item-input');
+    const todoListElement = this.#container.querySelector('ul');
+    const enterIsPressed = evt.code === KEY_CODE.ENTER;
+
+    if (enterIsPressed) {
+      this.add(newItemInput.value);
+      todoListElement.insertAdjacentHTML(
+        POSITION.BEFORE_END,
+        this.#todoItems[this.#todoItems.length - 1].getMarkup()
+      );
+      newItemInput.value = '';
+    }
+  };
+
+  add(...items) {
+    this.#todoItems = [
+      ...this.#todoItems,
+      ...items.map((item) => new TodoItem(item)),
+    ];
   }
 
   render() {
     let todoItemsMarkup = '';
-    if (this.#todoItemComponents.length) {
-      todoItemsMarkup = this.#todoItemComponents
-        .map((component) => component.getMarkup())
+    if (this.#todoItems.length) {
+      todoItemsMarkup = this.#todoItems
+        .map((item) => item.getMarkup())
         .join('');
     }
 
     this.#container.insertAdjacentHTML(
-      'afterbegin',
+      POSITION.BEFORE_END,
+      this.#getNewItemInputGroupMarkup()
+    );
+    this.#container.insertAdjacentHTML(
+      POSITION.BEFORE_END,
       this.#getTodoListMarkup(todoItemsMarkup)
     );
 
     this.#container.addEventListener('click', this.#listItemClickHandler);
+    this.#container.addEventListener('keydown', this.#enterKeydownHandler);
   }
 }
---
+++ js/consts.js
@@ -0,0 +1,10 @@
+export const POSITION_TO_INSERT = {
+  BEFORE_BEGIN: 'beforebegin',
+  AFTER_BEGIN: 'afterbegin',
+  BEFORE_END: 'beforeend',
+  AFTER_END: 'afterend',
+}
+
+export const KEY_CODE = {
+  ENTER: 'Enter',
+};
